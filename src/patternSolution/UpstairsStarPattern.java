cc18bc335aa2ff0addffb13f32bf6d8469b257cb
vinay-k-agarwal <vinay.agarwal@yabx.co>
2025-08-18 23:14:41 +0530

    more pattern added

---
+++ src/patternSolution/UpstairsStarPattern.java
@@ -0,0 +1,16 @@
+package patternSolution;
+
+public class UpstairsStarPattern {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = i + 1; j < 5; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
