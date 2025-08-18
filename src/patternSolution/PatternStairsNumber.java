41cdccc09d563bb49b72886369b542ccb8048862
vinay-k-agarwal <vinay.agarwal@yabx.co>
2025-08-18 15:51:07 +0530

    More pattern Program

---
+++ src/patternSolution/PatternStairsNumber.java
@@ -0,0 +1,15 @@
+package patternSolution;
+
+public class PatternStairsNumber {
+
+	public static void main(String[] args) {
+		for(int i =0; i<5; i++) {
+			for(int j = 1; j<=i+1;j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
